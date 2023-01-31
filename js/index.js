e3df149cea25833a033a92499119410c05d1416a
soevielofficial <soevielofficial@gmail.com>
2023-02-01 02:10:44 +0700

    modified:   image/icon.png 	modified:   index.html 	modified:   js/index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
+// Version: 1.0
+// Author: @soevielofficial
+// Github: https://github.com/soevielofficial
+// E-mail: soevielofficial@gmail.com
+
 window.addEventListener("hashchange", () => window.history.pushState({}, "", '/'), {});
